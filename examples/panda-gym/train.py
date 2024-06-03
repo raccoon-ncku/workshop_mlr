c0dcd471621e6a56a06749ac9a51e759aeea9a2f
Chia-Ching Yen <ccyen@umich.edu>
2024-06-04 01:28:02 +0800

    feat: Update training script for PandaReach-v3 environment

--- examples/panda-gym/train.py
+++ examples/panda-gym/train.py
@@ -3,5 +3,10 @@ import panda_gym
 from stable_baselines3 import DDPG
 
 env = gym.make("PandaReach-v3")
-model = DDPG(policy="MultiInputPolicy", env=env)
-model.learn(30_000)
+model = DDPG(policy="MultiInputPolicy", env=env, verbose=1)
+model.learn(30_000, progress_bar=True)
+
+
+# Save the model
+model.save("ddpg_panda")
+print("Model saved")
