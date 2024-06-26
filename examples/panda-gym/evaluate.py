674cfa3f13ba7fcb785718cdcff7e8980ce75734
Chia-Ching Yen <ccyen@umich.edu>
2024-06-26 13:08:44 +0800

    remove not tested files

--- examples/panda-gym/evaluate.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-from stable_baselines3 import DDPG
-from stable_baselines3.common.evaluation import evaluate_policy
-import panda_gym
-
-env = gym.make("PandaPickAndPlace-v3")
-model = DDPG("MultiInputPolicy", env)
-evaluate_policy(model, env, n_eval_episodes=100, warn=False)
-env.close()
